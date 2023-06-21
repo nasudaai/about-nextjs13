a857006abf3c1c61275fe1e66b46bc8f537a6b8b
nasuda <nasuda@example.com>
2023-06-21 12:17:39 +0900

    add Link app <=> pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+const MvPages = () => <Link href="/page">Page in pages</Link>
+
 export default function Page() {
-  return <h1>Page from app</h1>
+  return (
+    <>
+      <h1>Page from app</h1>
+      <MvPages />
+    </>
+  ) 
 }
--- pages/page.jsx
+++ pages/page.jsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+const MvApp = () => <Link href="/">Page in app</Link>
+
 export default function Page() {
-  return <h1>Page from pages</h1>
+  return (
+    <>
+      <h1>Page from pages</h1>
+      <MvApp />    
+    </>
+  )
 }
